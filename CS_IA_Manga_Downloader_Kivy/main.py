aff67e0f9df1465f9670f38ad36c11b11251d68d
Dimitriy Kruglikov <33755321+Gaysha1337@users.noreply.github.com>
2021-02-02 17:57:53 +0100

    Update main.py to request Android Permissions

--- CS_IA_Manga_Downloader_Kivy/main.py
+++ CS_IA_Manga_Downloader_Kivy/main.py
@@ -28,6 +28,7 @@ from MangaReader import MangaReaderChapterSelection, MangaReaderCarousel # This 
 
 # Utils
 from utils import create_language_dirs, create_root_dir, move_manga_root, resource_path, show_confirmation_dialog, create_screen
+from kivy.utils import platform
 
 # Setting a default font
 from kivy.core.text import LabelBase, DEFAULT_FONT
@@ -116,6 +117,11 @@ class MangaDownloader(MDApp):
         screen = MangaScreen(name="Landing Page")
         screen.add_widget(self.landing_page)
         self.screen_manager.add_widget(screen)
+        
+        # Android Permissions:
+        if platform == "android":
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
         
         return self.screen_manager
 
